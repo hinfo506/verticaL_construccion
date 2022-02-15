9416542dff838a747dc115c2b85addb8c786c394
dmontero <dmontero@quanam.com>
2022-02-15 14:57:29 -0300

    inicio

---
+++ project_capitulos/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Patrick Wilson <patrickraymondwilson@gmail.com>
+# License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+
+{
+    "name": "Project Capitulos",
+    "summary": """Project Capitulos""",
+    "author": "David Montero Crespo",
+    "website": "https://odoonext.com",
+    "category": "Project Management",
+    "version": "14.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/project.xml"],
+    "application": False,
+}
