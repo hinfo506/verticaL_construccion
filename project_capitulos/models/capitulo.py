2c7e4c9a9b8220247f4f6ebec9922558fb98f439
Raul Rolando Jardinot Gonzalez <rauljardinot@gmail.com>
2022-02-16 00:13:08 +0100

    update

--- project_capitulos/models/capitulo.py
+++ project_capitulos/models/capitulo.py
@@ -10,3 +10,9 @@ class Capitulo(models.Model):
     fecha_finalizacion = fields.Date('Fecha Finalización')
     project_id = fields.Many2one('project.project', string='Proyecto')
     descripcion = fields.Text('Descripción del Capitulo')
+
+    item_subcapitulo_ids = fields.One2many(
+        comodel_name='item.capitulo',
+        inverse_name='capitulo_id',
+        string='Item capitulos',
+        required=False)
