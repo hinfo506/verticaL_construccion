644a67160b4fc64e207af1926ca9db79587f0065
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-02-16 13:49:35 -0500

    con el contador de capitulos en proyecto

--- project_capitulos/models/project_inherit.py
+++ project_capitulos/models/project_inherit.py
@@ -6,6 +6,12 @@ class ProyectosInherit(models.Model):
     _inherit = 'project.project'
 
     capitulos_id = fields.One2many(comodel_name='capitulo.capitulo', inverse_name='project_id', string='Capitulos_id', required=False)
+    capitulos_count = fields.Integer(string='Contador de Capitulos', compute='get_count_capitulos')
+
+    def get_count_capitulos(self):
+        count = self.env['capitulo.capitulo'].search_count([('project_id', '=', self.id)])
+        self.capitulos_count = count
+
 
     def met_capitulos(self):
         # raise ValidationError("estoy dentrop")
