d5cff182a5806244275c002ff13fc7ca7b8e26f9
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-02-16 13:14:33 -0500

    update

--- project_capitulos/models/project_inherit.py
+++ project_capitulos/models/project_inherit.py
@@ -5,5 +5,17 @@ from odoo.exceptions import UserError, ValidationError
 class ProyectosInherit(models.Model):
     _inherit = 'project.project'
 
+    capitulos_id = fields.One2many(comodel_name='capitulo.capitulo', inverse_name='project_id', string='Capitulos_id', required=False)
+
     def met_capitulos(self):
-        raise ValidationError("estoy dentrop")
+        # raise ValidationError("estoy dentrop")
+        # self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Capitulos',
+            'res_model': 'capitulo.capitulo',
+            'view_mode': 'tree,form',
+            # 'domain': [('id', 'in', self.project_id.ids)],
+            # 'context': dict(self._context, default_vehiculo=self.vehicle_id.id, default_inscription_id=self.id,
+            #                 default_partner_id=self.purchaser_id.id)
+        }
