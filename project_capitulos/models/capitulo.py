0361ab8ef4bd80923778c260d6d1121ca3f9ced9
Raul Rolando Jardinot Gonzalez <rauljardinot@gmail.com>
2022-02-16 16:15:09 +0100

    agregado boton y page

--- project_capitulos/models/capitulo.py
+++ project_capitulos/models/capitulo.py
@@ -10,9 +10,25 @@ class Capitulo(models.Model):
     fecha_finalizacion = fields.Date('Fecha Finalización')
     project_id = fields.Many2one('project.project', string='Proyecto')
     descripcion = fields.Text('Descripción del Capitulo')
-
+    sub_count = fields.Integer(string='subc_count', required=False)
     subcapitulo_ids = fields.One2many(
         comodel_name='sub.capitulo',
         inverse_name='capitulo_id',
         string='Subcapitulos',
         required=False)
+
+    def subcapitulos_count(self):
+        count = self.env['sub.capitulo'].search_count([('subcapitulo_ids', '=', self.id)])
+        self.sub_count = count
+
+    def met_subcapitulos(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Subcapitulos',
+            'res_model': 'sub.capitulo',
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', self.subcapitulo_ids.ids)],
+            'context': dict(self._context, default_subcapitulo_ids=self.id),
+            # 'context': dict(self._context, default_vehiculo=self.vehicle_id.id, default_inscription_id=self.id,
+            #                 default_partner_id=self.purchaser_id.id)
+        }
