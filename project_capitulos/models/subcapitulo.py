0668e24e59e1362ba4d5b704b73cdb0c76628f8f
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-02-18 11:15:31 -0500

    agregado campos total

--- project_capitulos/models/subcapitulo.py
+++ project_capitulos/models/subcapitulo.py
@@ -1,5 +1,23 @@
 from odoo import fields, models, api
+# import logging
+# import pytz
+# import threading
+# from collections import OrderedDict, defaultdict
+# from datetime import date, datetime, timedelta
+# from psycopg2 import sql
+#
+# from odoo import api, fields, models, tools, SUPERUSER_ID
+# from odoo.addons.iap.tools import iap_tools
+# from odoo.addons.mail.tools import mail_validation
+# from odoo.addons.phone_validation.tools import phone_validation
+# from odoo.exceptions import UserError, AccessError
+# from odoo.osv import expression
+# from odoo.tools.translate import _
+# from odoo.tools import date_utils, email_re, email_split, is_html_empty
 
+# from . import crm_stage
+
+# _logger = logging.getLogger(__name__)
 
 class Subcapitulo(models.Model):
     _name = 'sub.capitulo'
@@ -9,6 +27,14 @@ class Subcapitulo(models.Model):
     total = fields.Float('Importe Total')
     fecha_finalizacion = fields.Date('Fecha Finalización')
     capitulo_id = fields.Many2one('capitulo.capitulo', string='Capitulo')
+
+    number = fields.Char(string='Number', required=True, copy=False,
+                       default=lambda self: self.env['ir.sequence'].next_by_code('task.lfpv'))
+
+    material_total = fields.Float(string='Total Coste Materiales', readonly='True')
+    labor_total = fields.Float(string='Total Coste Horas', readonly='True')
+    overhead_total = fields.Float(string='Total Costes Generales', readonly='True')
+    jobcost_total = fields.Float(string='Total Coste', readonly='True')
 
     item_capitulo_materiales_ids = fields.One2many(
         comodel_name='item.capitulo',
