8bf333244e31323e5d9b006071b70f9affe624de
dmontero <dmontero@quanam.com>
2022-03-09 10:18:35 -0300

    fix

--- on_dms_project/models/project_inherit.py
+++ on_dms_project/models/project_inherit.py
@@ -7,16 +7,20 @@ class Proyecto(models.Model):
 
     directorios_id = fields.One2many(comodel_name='dms.directory', inverse_name='project_id', string='Directorios',
                                    required=False)
+
+    directory_count = fields.Integer(string='Contador de Capitulos', compute='get_count_capitulos')
+
+    def get_count_capitulos(self):
+        count = self.env['dms.directory'].search_count([('project_id', '=', self.id)])
+        self.directory_count = count
+
+
     def met_directorios(self):
-        # raise ValidationError("estoy dentrop")
-        # self.ensure_one()
         return {
             'type': 'ir.actions.act_window',
             'name': 'Directorios',
             'res_model': 'dms.directory',
             'view_mode': 'tree,form',
-            # 'domain': [('id', 'in', self.directorios_id.ids)],
-            # 'context': dict(self._context, default_project_id=self.id),
-            # 'context': dict(self._context, default_vehiculo=self.vehicle_id.id, default_inscription_id=self.id,
-            #                 default_partner_id=self.purchaser_id.id)
+            'domain': [('id', 'in', self.directorios_id.ids)],
+            'context': dict(self._context, default_project_id=self.id),
         }
