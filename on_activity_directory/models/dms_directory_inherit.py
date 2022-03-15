a2e66e5190782c38fa49d37540128735865b157f
dmontero <dmontero@quanam.com>
2022-03-15 18:21:26 -0300

    fix

--- on_activity_directory/models/dms_directory_inherit.py
+++ on_activity_directory/models/dms_directory_inherit.py
@@ -6,14 +6,11 @@ from odoo import fields, models, api
 class Directorios(models.Model):
     _inherit = 'dms.directory'
 
-    # activities_id = fields.Many2one('mail.activity', string='Actividades')
-    activities_id = fields.One2many(comodel_name='mail.activity', inverse_name='directory_id', string='Actividades', required=False)
-
     activities_count = fields.Integer(string='Directorios', compute='get_activities_count')
 
     def get_activities_count(self):
         for r in self:
-            count = self.env['mail.activity'].search_count([('directory_id', '=', self.id)])
+            count = self.env['mail.activity'].search_count([('res_id', '=', self.id)])
             r.activities_count = count if count else 0
 
     def met_actividades(self):
@@ -22,6 +19,6 @@ class Directorios(models.Model):
             'name': 'Actividades',
             'res_model': 'mail.activity',
             'view_mode': 'kanban,tree,form',
-            'domain': [('id', 'in', self.activities_id.ids)],
-            'context': dict(self._context, default_directory_id=self.id),
+            'domain': [('res_id', '=',  self.id)],
+            #'context': dict(self._context, default_directory_id=self.id),
         }
