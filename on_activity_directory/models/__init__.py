2162d6a38d973894751a818ec7d02d4afec30055
dmontero <dmontero@quanam.com>
2022-03-16 10:20:36 -0300

    fix

--- on_activity_directory/models/__init__.py
+++ on_activity_directory/models/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from . import dms_directory_inherit
-from . import mail_activities_inherit
--- on_activity_directory/models/mail_activities_inherit.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api
-
-
-class MailActivities(models.Model):
-    _inherit = 'mail.activity'
-
-    directory_id = fields.Many2one(comodel_name='dms.directory', string='Directorios', required=False)
