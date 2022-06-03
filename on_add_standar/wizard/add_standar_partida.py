a8d04a89800c13619278f4300dbb206ec571634b
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-06-03 13:06:23 -0400

    update

--- on_add_standar/wizard/add_standar_partida.py
+++ on_add_standar/wizard/add_standar_partida.py
@@ -4,7 +4,7 @@ from odoo import fields, models, api
 class AddStandar(models.TransientModel):
     _name = 'add.standar'
 
-    standard_id = fields.Many2one(comodel_name='standard', string='Standar', required=False)
+    standard_id = fields.Many2one(comodel_name='standard', string='Standard', required=False)
     cant_partidas = fields.Integer(string='Cantidad Partidas', required=False)
     # line_ids = fields.One2many('standard.request.line', 'request_id')
     line_ids = fields.Many2many(comodel_name='standard.line', string='Line_ids')
