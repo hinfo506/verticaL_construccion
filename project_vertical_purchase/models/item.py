4993a7e2f391ac63e1b02fc8b53301f4eb16e4db
Micael Gomez <mikael85@gmail.com>
2022-09-13 19:23:51 -0300

    [ADD] purchase from stage

--- project_vertical_purchase/models/item.py
+++ project_vertical_purchase/models/item.py
@@ -11,32 +11,9 @@ class VerticalItem(models.Model):
     amount_confirm = fields.Float(string='Amount_confirm', required=False)
     amount_delivered = fields.Float(string='Amount_delivered', required=False)
 
-    # def purchase_from_item(self):
-    #     act_ids = self.env.context.get('active_ids')
-    #     item_ids = self.env['vertical.item'].search([('id', '=', act_ids)])
-    #     # raise ValidationError(item_ids[0].vertical_stage_id)
-    #     #
-    #     # for i in item_ids:
-    #     #     if i.stage_id != item_ids[0].stage_id:
-    #     #         pass
-    #
-    #     purchase = self.env['purchase.order'].create({
-    #         'partner_id': self.env.user.id,
-    #         'stage_id': item_ids[0].vertical_stage_id.id,
-    #     })
-    #     for i in item_ids:
-    #         # for i in item.item_ids:
-    #         purchase_line = self.env['purchase.order.line'].create({
-    #             'order_id': purchase.id,
-    #             'product_id': i.product_id.id,
-    #             'name': 'esta es la descripcion',
-    #             'product_qty': i.product_qty,
-    #             'price_unit': i.cost_price,
-    #             'item_id': i.id,
-    #         })
-
-
+    @api.model
     def purchase_from_item(self):
+        company_id = self.env.user.company_id
         po_obj = 'purchase.order'
         act_ids = self.env.context.get('active_ids',[])
         items = self.browse(act_ids)
@@ -48,9 +25,9 @@ class VerticalItem(models.Model):
             ('product_tmpl_id','in', product_ids.mapped('product_tmpl_id'))
         ])
         # Por cada proveedor, debo revisar si tengo al menos una PO en borrador y las guardo en un diccionario
+        _logger.info('vendors: ')
+        _logger.info(vendors)
         purchase_data = {}
-        purchase_orders = {}
-        purchase_order_lines = {}
         for vendor in vendors:
             po = po_obj.search([('partner_id', '=', vendor.name.id)], limit=1)
             p_data = {}
@@ -59,12 +36,15 @@ class VerticalItem(models.Model):
                 p_data['purchase_order'] = po[0].id
             p_data['order_lines'] = []
             purchase_data[vendor.name.id] = p_data
+        _logger.info('purchase_data: ')
+        _logger.info(purchase_data)
         # por cada producto, debo verificar las cantidades minimas del vendor y proceder
         for item in items:
             # Me interesan solo los que esten activos
             for seller in item.product.seller_ids.filtered(lambda s: s.name.active):
                 # revisar en la configuración del seller las cantidades y comparar que sea mayor o igual
                 if seller.min_qty <= item.product_qty:
+                    taxes = item.product_id.supplier_taxes_id.filtered(lambda x: x.company_id.id == company_id.id)
                     product_lang = item.product_id.with_prefetch().with_context(
                         lang=seller.name.lang,
                         partner_id=seller.name.id,
@@ -76,59 +56,40 @@ class VerticalItem(models.Model):
                         'name': name,
                         'product_qty': item.product_qty,
                         'product_id': item.product_id.id,
-                        'product_uom': product_id.uom_po_id.id,
-                        'price_unit': price_unit,
-                        'date_planned': date_planned,
+                        'product_uom': item.product_id.uom_po_id.id,
+                        'price_unit': seller.price,
+                        # 'date_planned': date_planned, #Implementar despues
                         'taxes_id': [(6, 0, taxes.ids)],
-                        'order_id': po.id,
+                        # 'order_id': po.id,
+                        'item_id': item.id
                     }
-                    new_po_line = self.env['purchase.order.line'].create(po_line_vals)
-                    purchase_data[seller.name.id]['order_lines'].append(new_po_line.id)
+                    purchase_data[seller.name.id]['order_lines'].append((0, False, po_line_vals))
 
         # Voy a ciclar por las lineas creadas, que estan agrupadas en el diccionario
-        for po_lines_key in purchase_order_lines.keys():
+        purchase_orders = []
+        for vendor_key in purchase_data.keys():
             # Ya existe una purchase order, no debo crearla
             current_po = False
-            must_update_dict = False
-            if po_lines_key in purchase_orders:
+            if 'purchase_order' in purchase_data[vendor_key]:
                 # Busco en la DB
-                current_po = po_obj.browse(purchase_orders[po_lines_key])
-                must_update_dict = True
+                current_po = po_obj.browse(purchase_data[vendor_key]['purchase_order'])
             else:
                 #debo crear una nueva PO para el proveedor seleccionado
                 current_po = po_obj.create({
-                    'partner_id': po_lines_key, # mi identificador del diccionario es el id del proveedor, lo obtuve arriba
-                    'stage_id': item_ids[0].vertical_stage_id.id,
+                    'partner_id': vendor_key, # mi identificador del diccionario es el id del proveedor, lo obtuve arriba
+                    # 'stage_id': item_ids[0].vertical_stage_id.id, # Para que quiero el stage?
                 })
-            for pol_id in purchase_order_lines[po_lines_key]:
-                # Agrego cada linea creada a la PO correspondiente.
-                current_po.order_line = [(4, pol_id)]
-            if must_update_dict:
-                purchase_orders[po_lines_key] = current_po.id
-        
+                purchase_data[vendor_key]['purchase_order'] = current_po.id #Actualizo mi dict
 
+            current_po.order_line = purchase_data[vendor_key]['order_lines']
+            purchase_orders.append(current_po.id)
 
+        # Debo retornar la acción que abre las purchase orders, pero mostrando solo las PO creadas
 
+        action = self.env["ir.actions.actions"]._for_xml_id("purchase.purchase_rfq")
+        action['domain'] = [('id', 'in', purchase_orders)]
+        action['context'] = {
+            'create': False,
+        }
+        return action
 
-        
-        # purchase_order.order_line = [(4, new_po_line.id)]
-
-        # item_ids = self.env['vertical.item'].search([('id', 'in', act_ids)])
-
-        # raise ValidationError(item_ids[0].vertical_stage_id)
-        #
-        # for i in item_ids:
-        #     if i.stage_id != item_ids[0].stage_id:
-        #         pass
-
-        # purchase = self.env['purchase.order'].create({
-        #     'partner_id': self.env.user.id,
-        #     'stage_id': item_ids[0].vertical_stage_id.id,
-        # })
-        # product.template
-
-        data = []
-        for i in item_ids:
-            data += i.product_id.seller_ids.name
-
-        raise ValidationError(data)
