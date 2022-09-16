c9a5b1692035a8c97dc276ec564430d24d59135a
Micael Gomez <mikael85@gmail.com>
2022-09-16 19:00:01 -0300

    [IMP] Full Data Structure

--- project_vertical_building/static/src/js/psi_view/psi_view.js
+++ project_vertical_building/static/src/js/psi_view/psi_view.js
@@ -11,17 +11,40 @@ class ProjectStageItemModel extends Model {
 
   setup(params, { orm }) {
     this.model = params.resModel;
-    console.log(this.model); //vertical.stage
+    // console.log(this.model); //vertical.stage
     this.orm = orm;
     this.keepLast = new KeepLast();
   }
 
   async load(params) {
-    this.data = await this.keepLast.add(
-      this.orm.searchRead(this.model, params.domain, [], { limit: 100 })
+    const self = this;
+    const domain = params.domain;
+    if ('project_id' in params.context){
+      domain.push(['project_id', 'in', params.context.project_id]);
+    }
+    const rawData = await this.keepLast.add(
+      this.orm.searchRead(this.model, domain, [], { limit: 1000 })
     );
+    this.rawData = rawData;
+    const projectStageItemData = _.map(_.uniq(_.map(rawData, (item) => item.project_id[0])), (project_id) => {return {
+      'id': false,
+      'project_id': project_id,
+      'vertical_stage_ids': _.filter(rawData, (stage) => stage.project_id[0] === project_id && stage.parent_id === false),
+    }});
+    projectStageItemData.forEach(
+      (projectStageItem) => projectStageItem.vertical_stage_ids.forEach(
+        (stage) => self._create_stages_tree(stage)));
+    console.log(projectStageItemData);
+    this.data = rawData;
     this.notify();
   }
+
+  _create_stages_tree(stagesTree){
+    const self = this;
+    stagesTree.vertical_stage_ids = _.filter(self.rawData, (stage) => stage.parent_id[0] === stagesTree.id);
+    _.map(stagesTree.vertical_stage_ids, (currentStage) => self._create_stages_tree(currentStage));
+  }
+
 }
 
 class ProjectStageItemView extends owl.Component {
@@ -31,6 +54,7 @@ class ProjectStageItemView extends owl.Component {
       domain: this.props.domain,
     });
     let searchModel = this.env.searchModel;
+    // console.log(this);
     searchModel.display = {
       controlPanel: false,
       searchPanel: false,
@@ -74,5 +98,10 @@ ProjectStageItemView.multiRecord = true;
 ProjectStageItemView.searchMenuTypes = ["filter", "favorite"];
 ProjectStageItemView.components = { Layout };
 ProjectStageItemView.template = "project_vertical_view.Layout";
+ProjectStageItemView.subTemplates = {
+  project: "project_vertical_view.Project",
+  stage: "project_vertical_view.Stage",
+};
+
 
 registry.category("views").add("project_stage_item", ProjectStageItemView);
