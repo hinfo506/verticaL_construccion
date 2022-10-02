ac4b6753388188d3b820aa1e87902f3ff3d29b6f
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-10-02 13:52:56 -0400

    update

--- project_vertical_purchase_catalogue/models/catalogue.py
+++ project_vertical_purchase_catalogue/models/catalogue.py
@@ -6,3 +6,9 @@ class VerticalCatalogue(models.Model):
     _name = 'vertical.catalogue'
 
     name = fields.Char('Name')
+
+    line_catalogue_ids = fields.One2many(
+        comodel_name='vertical.line.catalogue',
+        inverse_name='catalogue_id',
+        string='Line_catalogue_ids',
+        required=False)
--- project_vertical_purchase_catalogue/models/line_catalogue.py
+++ project_vertical_purchase_catalogue/models/line_catalogue.py
@@ -1,8 +1,9 @@
 from odoo import fields, models, api
 
 
-class ModelName(models.Model):
-    _name = 'ProjectName.TableName'
-    _description = 'Description'
+class VerticalLineCatalogue(models.Model):
+    _name = 'vertical.line.catalogue'
 
     name = fields.Char()
+
+    catalogue_id = fields.Many2one(comodel_name='vertical.catalogue', string='Catalogue_id', required=False)
