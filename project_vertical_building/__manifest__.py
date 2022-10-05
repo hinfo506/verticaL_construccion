8f6816f4802eada03a2b49c2b72faafe2639dc69
david <davidmonterocrespo24@gmail.com>
2022-10-05 11:37:15 -0300

    Update __manifest__.py

--- project_vertical_building/__manifest__.py
+++ project_vertical_building/__manifest__.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "project_vertical_building",
-
     'summary': """
         project_vertical_building""",
-
     'description': """
         project_vertical_building
     """,
-
     'author': "OdooNext",
     'website': "http://www.odoonext.com",
-
     'category': 'Project Management',
     'version': '15.0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ["web", "project", "stock", 'sh_activities_management_basic', 'base_setup', 'mail'],
-
-    # always loaded
     'data': [
-        # security
         'security/ir.model.access.csv',
-        # views
         'views/project_inherit.xml',
         'views/stage_views.xml',
         'views/view_stage_type.xml',
@@ -31,14 +21,11 @@
         'views/sequence.xml',
         'views/menu.xml',
     ],
-    # only loaded in demonstration mode
-
     'assets': {
         'web.assets_qweb': [
             'project_vertical_building/static/src/xml/*',
         ],
         'web.assets_common': [
-            # 'project_vertical_building/static/src/js/lib/bootstrap-menu.js',
             'project_vertical_building/static/src/js/lib/bootstrap-menu.min.js',
         ],
         'web.assets_backend': [
@@ -47,8 +34,6 @@
             'project_vertical_building/static/src/js/legacy/vertical_stage_tree.js',
             'project_vertical_building/static/src/js/psi_view/psi_view.js',
             'project_vertical_building/static/src/css/psi_view.scss',
-            # 'project_vertical_building/static/src/js/vertical_search_panel.js',
-            # 'project_vertical_building/static/src/js/vertical_stage_tree.js',
         ],
     }
 }
