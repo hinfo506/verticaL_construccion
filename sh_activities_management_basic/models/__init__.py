86a1c56508c820e6761af96b86cccb3233925ffa
david <davidmonterocrespo24@gmail.com>
2022-10-05 10:57:52 -0300

    eliminar aplicaciones de terceros

--- sh_activities_management_basic/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
-
-from . import activity_config_setting
-from . import activity_tags
-from . import ir_model
-from . import mail_activity
