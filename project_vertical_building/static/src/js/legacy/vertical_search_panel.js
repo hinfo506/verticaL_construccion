a2bf25d37f3e557fcac8cd1c6199fa82b89f51c6
david <davidmonterocrespo24@gmail.com>
2022-10-05 23:48:28 -0300

    clean js

--- project_vertical_building/static/src/js/legacy/vertical_search_panel.js
+++ project_vertical_building/static/src/js/legacy/vertical_search_panel.js
@@ -1,14 +1,11 @@
-odoo.define("project_vertical_building.search_panel", function (require) {
-  "use strict";
+odoo.define("project_vertical_building.search_panel", function (require) {"use strict";
 
-  const SearchPanel = require("web.searchPanel"); //FOR LEGACY
-  // const { SearchPanel } = require("@web/search/search_panel/search_panel"); //FOR NEW OWL
+  const SearchPanel = require("web.searchPanel");
 
   class search_panel extends SearchPanel {
   }
+
   search_panel.modelExtension = "VerticalSearchPanelMO";
-
   search_panel.template = "project_vertical_building.Legacy.SearchPanel";
-
   return search_panel;
 });
