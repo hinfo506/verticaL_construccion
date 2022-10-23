1f93853fb0483a3248c422800d2121665c841773
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-10-23 13:05:13 -0400

    update

--- project_vertical_cost_analysis/models/stage.py
+++ project_vertical_cost_analysis/models/stage.py
@@ -1,9 +1,12 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class VerticalStage(models.Model):
     _inherit = 'vertical.stage'
 
+
+    cost_analysis_id = fields.Many2one(comodel_name='vertical.cost.analysis',string='Cost_analysis_id', required=False)
     itemcost_count = fields.Integer(string='Itemcost_count', required=False, compute='get_item_cost_count')
 
     def get_item_cost_count(self):
@@ -29,3 +32,81 @@ class VerticalStage(models.Model):
             #     default_project_id=self.project_id.id,
             # ),
         }
+
+    # def action_insertar(self):
+    #     for line in self.list_ids:
+    #         # raise ValidationError(self.active_id)
+    #         self.env["vertical.item"].create(
+    #             {
+    #                 "vertical_stage_id": self.active_id.id,
+    #                 "project_id": self.active_id.project_id.id,
+    #                 "type_item": 'standard',
+    #                 # "standar_id": self.id,
+    #                 "job_type": line.job_type,
+    #                 "product_id": line.product_id.id,
+    #                 "descripcion": line.descripcion,
+    #                 "uom_id": line.uom_id.id,
+    #                 "product_qty": line.product_qty,
+    #                 "cost_price": line.cost_price,
+    #                 "subtotal_item_capitulo": line.subtotal_item_capitulo,
+    #                 "tipo_descuento": line.tipo_descuento,
+    #                 "cantidad_descuento": line.cantidad_descuento,
+    #                 "subtotal_descuento": line.subtotal_descuento,
+    #                 "impuesto_porciento": line.impuesto_porciento,
+    #                 "total_impuesto_item": line.total_impuesto_item,
+    #                 "beneficio_estimado": line.beneficio_estimado,
+    #                 "importe_venta": line.importe_venta,
+    #                 "suma_impuesto_item_y_cost_price": line.suma_impuesto_item_y_cost_price,
+    #             }
+    #         )
+
+    @api.model
+    def create(self, vals):
+        record = super(VerticalStage, self).create(vals)
+        if (
+                record.project_id
+                and record.project_id.stage_id
+                and record.project_id.stage_id.is_prevision
+        ):
+            state = (
+                "aprobada" if record.project_id.stage_id.is_prevision else "pendiente"
+            )
+            record.write({"estado_fase": state})
+
+
+        for line in record.cost_analysis_id.cost_analysis_line_ids:
+            # raise ValidationError(self.active_id)
+            record.env["vertical.item"].create(
+                {
+                    "vertical_stage_id": record.id,
+                    "project_id": record.project_id.id,
+                    "type_item": 'cost_analysis',
+                    # "standar_id": self.id,
+                    "job_type": line.job_type,
+                    "product_id": line.product_id.id,
+                    "descripcion": line.descripcion,
+                    "uom_id": line.uom_id.id,
+                    "product_qty": line.product_qty,
+                    "cost_price": line.cost_price,
+                    "subtotal_item_capitulo": line.subtotal_item_capitulo,
+                    "tipo_descuento": line.tipo_descuento,
+                    "cantidad_descuento": line.cantidad_descuento,
+                    "subtotal_descuento": line.subtotal_descuento,
+                    "impuesto_porciento": line.impuesto_porciento,
+                    "total_impuesto_item": line.total_impuesto_item,
+                    "beneficio_estimado": line.beneficio_estimado,
+                    "importe_venta": line.importe_venta,
+                    "suma_impuesto_item_y_cost_price": line.suma_impuesto_item_y_cost_price,
+                }
+            )
+        return record
+
+    def write(self, values):
+        record = super(VerticalStage, self).write(values)
+        if values.get("cost_analysis_id") == 0:
+            raise ValidationError('has editado el analisis de coste')
+        # if values.get("cost_analysis_id"):
+        #     raise ValidationError('has editado el analisis de coste')
+            return record
+        else:
+            return record
